f4069f3ac0798f8d5d420be9d0abcec906efa6ab
Sourav61 <sourav61pahwa@gmail.com>
2024-12-17 00:38:38 +0530

    Fix: Fixed the encryption algo to work for entire sentences Fixed the encryption algo to work for entire sentences

--- message_encrypt.py
+++ message_encrypt.py
@@ -2,26 +2,39 @@ import string
 import random
 
 def decode(str):
-    fchar = str[-4]
-    str = str[3:-4]
-    return fchar + str
+    if(len(str) >= 3):
+        fchar = str[-4]
+        str = str[3:-4]
+        return fchar + str
+
+    return str[::-1]
 
 def encode(str):
-    s1 = ''.join(random.choices(string.ascii_letters, k=3))
-    s2 = ''.join(random.choices(string.ascii_letters, k=3))
-    str = str[1:] + str[0]
-    return s1 + str + s2
+    if(len(str) >= 3):
+        s1 = ''.join(random.choices(string.ascii_letters, k=3))
+        s2 = ''.join(random.choices(string.ascii_letters, k=3))
+        str = str[1:] + str[0]
+        return s1 + str + s2
+
+    return str[::-1]
 
 message = input("Please enter your message: ")
 code_type = input("Please enter mechanism to be used: Encoding/Decoding: ")
 
+words = message.split(" ")
 # decode(message)
 
 if code_type.lower() == "encoding":
-    str = encode(message)
-    print("Encoded message:", str)
-    str = decode(str)
-    print("Decoded message:", str)
+    nwords = []
+    for word in words:
+        nwords.append(encode(word))
+
+    print("Encoded message:", " ".join(nwords))
+    
 else:
-    str = decode(message)
-    print("Decoded message:", str)
+    nwords = []
+    for word in words:
+        nwords.append(decode(word))
+
+    print("Decoded message:", " ".join(nwords))
+
